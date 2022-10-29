8ece50dfe0f6373add718962e9da8a18154860cc
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-10-28 18:27:56 -0700

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,35 @@
 jQuery(function ($) {
 
+    $(".galleryCar").owlCarousel({
+        nav: true,
+        loop: true,
+        dots: true,
+        center:true,
+        items: 1,
+        navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
+        margin: 10,
+        stagePadding: 100,
+        rtl:true,
+
+    });
+    $(".ourClientCar").owlCarousel({
+        nav: true,
+        loop: false,
+        items: 2,
+        rtl:true,
+        navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
+        margin: 130,
+        responsive:{
+            0:{
+                items:1,  margin: 0
+
+            },
+            700:{
+                items:2,
+            }
+        }
+
+    });
 
 
 });
