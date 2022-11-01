93ede6bdd0e8013f108e16e3af38c4464704efb6
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-11-01 12:26:02 -0700

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,32 +4,40 @@ jQuery(function ($) {
         nav: true,
         loop: true,
         dots: true,
-        center:true,
+        center: true,
         items: 1,
         navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
         margin: 10,
         stagePadding: 100,
-        rtl:true,
-
-    });
-    $(".ourClientCar").owlCarousel({
-        nav: true,
-        loop: false,
-        items: 2,
-        rtl:true,
-        navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
-        margin: 130,
-        responsive:{
-            0:{
-                items:1,  margin: 0
-
+        rtl: true,
+        responsive: {
+            0: {
+                stagePadding: 0,
             },
-            700:{
-                items:2,
+            800: {
+                stagePadding: 100,
             }
         }
-
     });
+$(".ourClientCar").owlCarousel({
+    nav: true,
+    loop: false,
+    items: 2,
+    rtl: true,
+    navText: ['<i class="fa fa-chevron-right"></i>', '<i class="fa fa-chevron-left"></i>'],
+    margin: 130,
+    responsive: {
+        0: {
+            items: 1,
+            margin: 0
+        },
+
+        700: {
+            items: 2,
+        }
+    }
+
+});
 
 
 });
